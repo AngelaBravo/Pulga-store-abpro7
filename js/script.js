c836bb0f3a3f7a9f0b8656c2cc9c50704ce01488
ABravo <angelabravop@gmail.com>
2025-06-26 16:45:35 -0400

    quite los botones quegrados ayer y ordene algunas cosas y agregue el readme

--- js/script.js
+++ js/script.js
@@ -1,37 +1,39 @@
 function actualizarFecha() {
-    const elementoFecha = document.getElementById("fecha");
-    const ahora = new Date();
+  const elementoFecha = document.getElementById("fecha");
+  const ahora = new Date();
 
-    const opciones = {
-      weekday: 'long', year: 'numeric', month: 'long',
-      day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'
-    };
+  const opciones = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
 
-    elementoFecha.textContent = ahora.toLocaleString('es-CL', opciones);
+  elementoFecha.textContent = ahora.toLocaleString("es-CL", opciones);
+}
+
+actualizarFecha();
+
+setInterval(actualizarFecha, 1000);
+
+const formulario = document.getElementById("formulario");
+const mensaje = document.getElementById("mensaje");
+
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre-completo").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const comentario = document.getElementById("comentario").value.trim();
+
+  if (!nombre || !email || !comentario) {
+    alert("Por favor, completa todos los campos.");
+    mensaje.style.display = "none";
+  } else {
+    mensaje.style.display = "block";
+    formulario.reset();
   }
-
-  actualizarFecha();
-
-  setInterval(actualizarFecha, 1000);
-
-  const formulario = document.getElementById('formulario');
-  const mensaje = document.getElementById('mensaje');
-
-  formulario.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    const nombre = document.getElementById('nombre-completo').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const comentario = document.getElementById('comentario').value.trim();
-
-    if (!nombre || !email || !comentario) {
-      alert('Por favor, completa todos los campos.');
-      mensaje.style.display = 'none'; // Oculta mensaje si ya estaba mostrado
-    } else {
-      // Mostrar el mensaje
-      mensaje.style.display = 'block';
-
-      // Limpiar el formulario
-      formulario.reset();
-    }
-  });
+});
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Pulga Store
+
+Tiene una pagina principal ( index) donde se muestra la historia del negocio y la ubicacion
+
+Luego tiene una pagina de contacto donde una persona puede contartarse con la empresa
+
+Y una tercera pagina donde se muestran los distintos productos en mayor detalle para que puedan elegir que comprar. Actualmente solo funciona la opcion electrodomesticos.
